63a1bf9e03ad6d9a886de96c85acac3634809156
Tim <50870607+toothless13@users.noreply.github.com>
2023-06-20 12:45:20 +0100

    solve fifth problem

--- src/numbers.js
+++ src/numbers.js
@@ -14,9 +14,13 @@ const divide = (a, b) => {
   return a / b;
 };
 
-function power (a, b) {
-  // your code here
-}
+// function power (a, b) {
+//   // your code here
+// }
+
+const power = (a, b) => {
+  return Math.pow(a, b);
+};
 
 function round (a) {
   // your code here
