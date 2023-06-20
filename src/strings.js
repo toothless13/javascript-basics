460fe279186040a9b13266aafcda444a079a92f0
Tim <50870607+toothless13@users.noreply.github.com>
2023-06-20 14:55:21 +0100

    solve sixth problem

--- src/strings.js
+++ src/strings.js
@@ -18,8 +18,12 @@ const firstCharacter = string => {
   return string[0];
 };
 
-function firstCharacters (string, n) {
-  // your code here
+// function firstCharacters (string, n) {
+//   // your code here
+// };
+
+const firstCharacters = (string, n) => {
+  return string.slice(0, n);
 };
 
 module.exports = {
